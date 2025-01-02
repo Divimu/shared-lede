787dc4c3bc3e056e9f1a0cebb9eb5908a9bf9985
Divimu <deng_9527@163.com>
2025-01-03 01:33:13 +0800

    Update 1.clone.sh

--- preset-lienol-openwrt/1.clone.sh
+++ preset-lienol-openwrt/1.clone.sh
@@ -3,7 +3,7 @@
 # download base code
 CODE_DIR=_firmware_code
 CODE_URL=https://github.com/Lienol/openwrt
-CODE_BRANCH=23.05
+CODE_BRANCH=24.10
 git clone --depth 1 -b $CODE_BRANCH $CODE_URL $CODE_DIR
 mv ./$CODE_DIR/* ./
 
@@ -15,6 +15,4 @@ git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/Ausaci/luci-app-nat6-helper.git
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
-git clone --depth 1 https://github.com/vernesong/OpenClash.git && mv OpenClash/luci-app-openclash ./ && rm -rf OpenClash
